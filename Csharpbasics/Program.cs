81b4ce61699fc5bf0b8f437ec8dce7698f45d855
Anjali Neupane <anjalilajna38@gmail.com>
2020-09-22 12:40:43 +0545

    More on files

--- Csharpbasics/Program.cs
+++ Csharpbasics/Program.cs
@@ -1,12 +1,19 @@
 ﻿using LearnCollections;
+using System;
+using System.IO;
+using System.Linq;
 
 public class Program
 {
     static void Main()
     {
-        FileIO fileIO = new FileIO();
-        fileIO.LearnFileHandling();
-        fileIO.LearnDirectories ();
-
+        string countriesText = File.ReadAllText ("Countries.txt");
+        string[] countries =countriesText.Split ("\r\n");
+        var countriesWithNIntial= countries.Where(x=> x.StartsWith("N")).Select(x=> x);
+        foreach (var country in countriesWithNIntial)
+        {
+            Console.WriteLine (country);
+            
+        }
     }
 }
