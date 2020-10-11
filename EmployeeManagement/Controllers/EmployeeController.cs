e7c9b3335c314671615f77328737c103ab981bac
Anjali Neupane <anjalilajna38@gmail.com>
2020-10-11 12:59:47 +0545

    Changes added in EMS

--- EmployeeManagement/Controllers/EmployeeController.cs
+++ EmployeeManagement/Controllers/EmployeeController.cs
@@ -5,12 +5,17 @@ public class EmployeeController : Controller
     public ActionResult Index()
     {
         //Object initializer syntax
-        Person empl1 = new Person() {  FirstName = "Anjali", SurName = "Neupane", Salary = 2000.0 };
-        Person empl2 = new Person() {  FirstName = "Anjali", SurName = "Neupane", Salary = 2000.0 };
-        Person empl3 = new Person() {  FirstName = "Anjali", SurName = "Neupane", Salary = 2000.0 };
-        Person empl4 = new Person() {  FirstName = "Anjali", SurName = "Neupane", Salary = 2000.0 };
-        Person empl5 = new Person() {  FirstName = "Anjali", SurName = "Neupane", Salary = 2000.0 };
+        Person empl1 = new Person() {  FirstName = "Anjali", SurName = "Neupane", Address="Syangja" ,Salary = 2000.0 };
+        Person empl2 = new Person() {  FirstName = "shisam", SurName = "Neupane", Address="Syangja", Salary = 2000.0 };
+        Person empl3 = new Person() {  FirstName = "Shreya", SurName = "Neupane", Address="Syangja" ,Salary = 2000.0 };
+        Person empl4 = new Person() {  FirstName = "Nisha", SurName = "Neupane" ,Address="Syangja" ,Salary = 2000.0 };
+        Person empl5 = new Person() {  FirstName = "Anjali", SurName = "Neupane" ,Address="Syangja" ,Salary = 2000.0 };
         List <Person> employees = new List<Person> (){ empl1, empl2, empl3, empl4, empl5};
         return View(employees);
     }
+    public ActionResult Detail (string firstName)
+    {
+        return View();
+    }
+
 }
--- EmployeeManagement/Models/Person.cs
+++ EmployeeManagement/Models/Person.cs
@@ -3,6 +3,6 @@ public class Person
     public string FirstName {get;set;}
     public string SurName{get;set;}
     public string Address{get;set;}
-    public char Gender{get;set;}='M';
+    public char Gender{get;set;}='F';
     public double Salary {get;set;}
 }  
