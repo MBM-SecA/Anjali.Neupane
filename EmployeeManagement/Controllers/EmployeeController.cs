a148f3c4d9b80e910bdaafaf5fa29cab371ec853
Anjali Neupane <anjalilajna38@gmail.com>
2020-10-15 10:46:28 +0545

    Contents Added

--- EmployeeManagement/Controllers/EmployeeController.cs
+++ EmployeeManagement/Controllers/EmployeeController.cs
@@ -1,21 +1,18 @@
-using System.Collections.Generic;
+using System;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 public class EmployeeController : Controller
 {
     public ActionResult Index()
     {
-        //Object initializer syntax
-        Person empl1 = new Person() {  FirstName = "Anjali", SurName = "Neupane", Address="Syangja" ,Salary = 2000.0 };
-        Person empl2 = new Person() {  FirstName = "shisam", SurName = "Neupane", Address="Syangja", Salary = 2000.0 };
-        Person empl3 = new Person() {  FirstName = "Shreya", SurName = "Neupane", Address="Syangja" ,Salary = 2000.0 };
-        Person empl4 = new Person() {  FirstName = "Nisha", SurName = "Neupane" ,Address="Syangja" ,Salary = 2000.0 };
-        Person empl5 = new Person() {  FirstName = "Anjali", SurName = "Neupane" ,Address="Syangja" ,Salary = 2000.0 };
-        List <Person> employees = new List<Person> (){ empl1, empl2, empl3, empl4, empl5};
+        var employees = Person.GetEmployees();
         return View(employees);
     }
-    public ActionResult Detail (string firstName)
+    public ActionResult Detail ([FromQuery]int id)
     {
-        return View();
+        var employees = Person.GetEmployees();
+        var employee = employees.FirstOrDefault(x => x.Id == id);
+        return View(employee);
     }
 
 }
--- EmployeeManagement/Models/Person.cs
+++ EmployeeManagement/Models/Person.cs
@@ -1,8 +1,23 @@
+using System;
+using System.Collections.Generic;
 public class Person
 {
+    public int Id { get; set; }
     public string FirstName {get;set;}
     public string SurName{get;set;}
     public string Address{get;set;}
     public char Gender{get;set;}='F';
     public double Salary {get;set;}
+
+    public static List <Person> GetEmployees()
+    {
+        //Object initializer syntax
+        Person empl1 = new Person() {Id=1, FirstName = "Anjali", SurName = "Neupane", Address="Syangja" ,Salary = 2000.0 };
+        Person empl2 = new Person() {Id=2, FirstName = "shisam", SurName = "Neupane", Address="Syangja", Salary = 2000.0 };
+        Person empl3 = new Person() {Id=3, FirstName = "Shreya", SurName = "Neupane", Address="Syangja" ,Salary = 2000.0 };
+        Person empl4 = new Person() {Id=4, FirstName = "Nisha", SurName = "Neupane" ,Address="Syangja" ,Salary = 2000.0 };
+        Person empl5 = new Person() {Id=5, FirstName = "Shrisha", SurName = "Neupane" ,Address="Syangja" ,Salary = 2000.0 };
+        List <Person> employees =  new List<Person>() { empl1, empl2, empl3, empl4, empl5};
+        return employees;
+    }
 }  
