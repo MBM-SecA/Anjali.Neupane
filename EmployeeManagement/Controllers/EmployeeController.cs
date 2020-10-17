3cafaf2955e04164ab101c231f8f0880729175b3
Anjali Neupane <anjalilajna38@gmail.com>
2020-10-17 13:57:14 +0545

    Dropdown added

--- EmployeeManagement/Controllers/EmployeeController.cs
+++ EmployeeManagement/Controllers/EmployeeController.cs
@@ -1,18 +1,29 @@
 using System;
 using System.Linq;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 public class EmployeeController : Controller
 {
+    public List<Person> employees = Person.GetEmployees();
     public ActionResult Index()
     {
-        var employees = Person.GetEmployees();
+        //var employees = Person.GetEmployees();
         return View(employees);
     }
-    public ActionResult Detail ([FromQuery]int id)
+    public ActionResult Detail (int id)
     {
         var employees = Person.GetEmployees();
         var employee = employees.FirstOrDefault(x => x.Id == id);
         return View(employee);
     }
-
+    
+    public ActionResult Add()
+    {
+        return View();
+    } 
+    [HttpPost]
+    public ActionResult <string> Add(Person person)
+    {
+        return "Record saved";
+    }
 }
--- EmployeeManagement/Models/Person.cs
+++ EmployeeManagement/Models/Person.cs
@@ -1,13 +1,18 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 public class Person
 {
     public int Id { get; set; }
+    [Required, Display (Name = "First Name")]
     public string FirstName {get;set;}
+    [Required (ErrorMessage="We need Last Name.")]
+    [Display(Name = "Last Name")]
+
     public string SurName{get;set;}
     public string Address{get;set;}
-    public char Gender{get;set;}='F';
-    public double Salary {get;set;}
+    public char? Gender{get;set;}='F';
+    public double? Salary {get;set;}
 
     public static List <Person> GetEmployees()
     {
