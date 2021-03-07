1b15e933b88802816057b5609bbe451b54db313d
Anjali Neupane <anjalilajna38@gmail.com>
2021-03-07 10:34:31 +0530

    HTTP Request methods

--- StudentsApi/Controllers/StudentsController.cs
+++ StudentsApi/Controllers/StudentsController.cs
@@ -1,4 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using StudentApi.Models;
+using System.Text.Json;
 [Route("students")]
 [ApiController]
 public class StudentsController: ControllerBase
@@ -14,5 +18,33 @@ public class StudentsController: ControllerBase
             return NotFound();
         }
         return Ok (students);
+    }
+    [HttpGet]
+    [Route("{name}")]
+    public ActionResult GetAllStudentsById( string name)
+    {
+       var students = new string [] {"Sagun","Sandhya"};
+        var student = students.Where(x => x== name).FirstOrDefault();
+        if (students == null)
+        {
+            return NotFound();
+        }
+        return Ok (student); 
+    }
+     [HttpPost]
+    [Route("add")]
+    public ActionResult CreateStudent ( Student student)
+    {
+        if (student == null)
+        {
+            return BadRequest();
+        }
+        //Add student to db
+       // return NoContent(); 
+       return Created ("", student);
+    }
+    private void FirstOrDefault()
+    {
+        throw new NotImplementedException();
     }
 }
